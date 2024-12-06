09627376b586fd2eb0ed79a7766ad97ddcbe8ab0
Alessandro Tuveri <at@specialistaweb.it>
2024-12-06 17:24:35 +0100

    compat version
    for bootstrap package and typo3 12-13

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array (
   'title' => 'image rollover collection 2 - CIRCLE',
   'description' => 'CIRCLE rollover effetcts from tympanus for cType textmedia, media, images, textpic - Only for bootstrap package 12+ with Bootstrap 5.x (SCSS) (bootstrap_package) library',
   'category' => 'plugin',
-  'version' => '7.0.5',
+  'version' => '8.0.0',
   'state' => 'stable',
   'uploadfolder' => false,
   'clearcacheonload' => false,
@@ -25,8 +25,8 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '11.0.0-12.4.99',
-      'bootstrap_package' => '12.0.0-14.0.99',
+	'typo3' => '12.0.0-13.4.99',
+      	'bootstrap_package' => '13.0.0-15.0.99',
     ),
     'conflicts' => 
     array (
